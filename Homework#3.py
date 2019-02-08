4ad8afae7d621c7e300675199e182ffbcd4e8ad1
UnaAdam <ms.dragica.adamovic@gmail.com>
2019-02-08 11:41:29 -0500

    Third commit

---
+++ Homework#3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+myfilename = "housing.data.txt"
+if os.path.isfile(myfilename):
+    print('yay, I have a file')
+    if sky == blue:
+        print('yay, the sku is blue')
+else:
+    print('boo, no files for me')
+
+
+
